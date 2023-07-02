a14102c62857b9c57d7569ffa72cd1b57044b0c2
anarakinson <anarakinson@gmail.com>
2023-07-02 23:52:17 +0300

    initial commit

---
+++ source/lexer/lexer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+
+class Lexer {
+public:
+    Lexer() {
+        std::cout << "lexer" << std::endl;
+    }
+
+};
---
+++ source/run_test.cpp
@@ -0,0 +1,15 @@
+#include <lexer.h>
+#include <parser.h>
+
+#include <iostream>
+#include <string>
+
+
+int main() {
+
+    std::cout << "START" << std::endl;
+
+    Lexer lexer{};
+    Parser parser{};
+
+}
